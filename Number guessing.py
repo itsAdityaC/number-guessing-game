a6224848f54d3a5c54d95d9f8c924dbe7f499064
itsAdityaC <adityachaurasia139@gmail.com>
2026-06-23 11:54:23 +0530

    Update Number guessing.py

--- Number guessing.py
+++ Number guessing.py
@@ -1,7 +1,7 @@
 import random
 
-# Generate a random number between 1 and 100
-secret_number = random.randint(1, 100)
+
+secret_number = random.randint(1, 100)   # Generate a random number between 1 and 100
 
 attempts = 0
 
